9a1d4035a30293c98489ada87968ce4029f60cc9
cholt18 <56280005+cholt18@users.noreply.github.com>
2019-10-07 19:10:51 -0400

    Update stairgen.cpp

--- stairgen.cpp
+++ stairgen.cpp
@@ -4,11 +4,11 @@
 
 int main(){
 
-  std::cout << "Please enter an integer";;
+  std::cout << "Please enter an integer" << std::endl;
   int n;
   std::cin >> n;
   std::stirng row;
-  for(unsigned int i = 0; i < n; i++){
+  for(unsigned int i = 1; i < n+1; i++){
     row = "X"*i;
     std::cout << row << std::endl;
   }
